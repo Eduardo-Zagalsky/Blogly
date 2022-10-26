c133b0484ed6faa5989b125086a3236bb073ccf8
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-10-25 20:58:23 -0500

    rearranged the lines hoping to get it to work

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "secret"
-debug = DebugToolbarExtension(app)
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///Blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+
+@app.route("/")
+def root():
+    return "<html><body><h1>Test</h1></body></html>"
