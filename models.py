f7d5e64656a3f0dedc7e12a7f8f509ca4a98c0c6
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-01 15:47:29 -0500

    deleted unique from image, added a model for posts

--- models.py
+++ models.py
@@ -15,8 +15,20 @@ class User(db.Model):
     first_name = db.Column(db.String(10), nullable=False)
     last_name = db.Column(db.String(10), nullable=False)
     image_url = db.Column(
-        db.Text, unique=True, default="https://cdn.vectorstock.com/i/preview-1x/66/14/default-avatar-photo-placeholder-profile-picture-vector-21806614.jpg")
+        db.Text, default="https://cdn.vectorstock.com/i/preview-1x/66/14/default-avatar-photo-placeholder-profile-picture-vector-21806614.jpg")
 
     def __repr__(self):
         s = self
         return f"<User {s.id} {s.first_name} {s.last_name}>"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    post_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column()
+    user_code = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='posts')
