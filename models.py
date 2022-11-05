7bc6f654b76a6212d8b755ae3c51ca88adb0a7bd
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-04 20:55:14 -0500

    added the post relationship, changed the default img, and added full name property. added time date to organize the time date info

--- models.py
+++ models.py
@@ -15,12 +15,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(10), nullable=False)
     last_name = db.Column(db.String(10), nullable=False)
-    image_url = db.Column(db.Text,
-                          default="https://cdn.vectorstock.com/i/preview-1x/66/14/default-avatar-photo-placeholder-profile-picture-vector-21806614.jpg")
+    image_url = db.Column(
+        db.Text, default="https://media.cheggcdn.com/media/d53/d535ce9a-4535-4e56-bd8e-81300a25a4f7/php4KwLCz")
+    posts = db.relationship("Post", backref="user",
+                            cascade="all, delete-orphan")
 
-    def __repr__(self):
-        s = self
-        return f"<User {s.id} {s.first_name} {s.last_name}>"
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Post(db.Model):
@@ -32,3 +34,7 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @property
+    def time_date(self):
+        return self.created_at.strftime("%m/%d/%Y, %H:%M:%S")
