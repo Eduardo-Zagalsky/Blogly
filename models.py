47293e53c60323667700b450a51885037154cbf1
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-06 18:15:20 -0600

    added tag model, posttag model, and tags relationship

--- models.py
+++ models.py
@@ -35,7 +35,22 @@ class Post(db.Model):
                            default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    tags = db.relationship("Tag", secondary="posttags", backref="post")
+
     @property
     def time_date(self):
         return self.created_at.strftime("%m/%d/%Y, %I:%M %p")
 
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+
+
+class PostTag(db.Model):
+    __tablename__ = "posttags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)
